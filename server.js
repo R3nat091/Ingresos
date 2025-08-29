53b62ebcc17eec10982f50200ece3c546c657863
R3nat091 <renato.balboa@asiamerica.cl>
2025-08-29 10:46:36 -0400

    mejora de conexion

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ const sql = require('mssql');
 require('dotenv').config();
 
 const dbConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER,
-  database: process.env.DB_DATABASE,
+  user: "adminsql",
+  password: "A$1s0p21",
+  server: "asiamerica.database.windows.net",
+  database: "asiamerica",
   port: 1433,
   options: {
-    encrypt: false,
-    trustServerCertificate: true
+    encrypt: true,
+    trustServerCertificate: false
   }
 };
 app.use(express.static(path.join(__dirname, 'public')));
